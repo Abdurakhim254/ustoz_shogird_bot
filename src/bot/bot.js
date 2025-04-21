04a39ec42f6f648920446ff86d4c92681b122e29
Abdurakhim254 <toxirmalk@gmail.com>
2025-04-21 10:46:31 +0500

    code has some optimization

--- src/bot/bot.js
+++ src/bot/bot.js
@@ -5,6 +5,7 @@ import { scenes } from "../scenes/index.js";
 import { MAIN_MESSAGES } from "../messages/main.messages.js";
 import { getcontact, Jobkeyboard, Addpost } from "../keyboards/index.js";
 import { APPLICATION } from "../config/index.js";
+import { messageDeleter } from "../helpers/functions/index.js";
 
 const token = APPLICATION.token;
 
@@ -17,15 +18,18 @@ bot.use(scenes);
 
 bot.command("start", async (ctx) => {
   const id = ctx.update.message.from.id;
+  ctx.session.messageIds=[]
   const result = await User.findOne({ user_id: id });
   if (!result) {
-    await ctx.reply(MAIN_MESSAGES.AskContact, { reply_markup: getcontact });
+    const message=await ctx.reply(MAIN_MESSAGES.AskContact, { reply_markup: getcontact });
+    ctx.session.messageIds.push(message.message_id)
     return;
   }
 
-  await ctx.reply(MAIN_MESSAGES.ToStartAdd, {
+ const message = await ctx.reply(MAIN_MESSAGES.ToStartAdd, {
     reply_markup: Addpost,
   });
+  ctx.session.messageIds.push(message.message_id)
 });
 
 bot.command("admin", async (ctx) => {
@@ -35,6 +39,7 @@ bot.command("admin", async (ctx) => {
 bot.on("message:contact", async (ctx) => {
   try {
     const contact = ctx.message.contact;
+    ctx.session.messageIds.push(ctx.update.message.message_id)
     const user = ctx.update.message?.from;
     const result = await User.findOne({ username: user.username });
     if (!result) {
@@ -46,9 +51,11 @@ bot.on("message:contact", async (ctx) => {
       });
     }
 
-    await ctx.reply(MAIN_MESSAGES.successmessage, {
+    const message = await ctx.reply(MAIN_MESSAGES.successmessage, {
       reply_markup: Addpost,
     });
+    ctx.session.messageIds.push(message.message_id)
+    await messageDeleter(ctx.session.messageIds)
   } catch (error) {
     await ctx.reply(error.message);
   }
@@ -58,9 +65,10 @@ bot.on("message:contact", async (ctx) => {
 
 bot.callbackQuery("addPost", async (ctx) => {
 
-  await ctx.reply(MAIN_MESSAGES.ToStartTypes, {
+  const message = await ctx.reply(MAIN_MESSAGES.ToStartTypes, {
     reply_markup: Jobkeyboard,
   });
+  ctx.session.messageIds.push(message.message_id)
 });
 
 bot.callbackQuery("sherik", async (ctx) => {
@@ -83,7 +91,3 @@ bot.callbackQuery("shogird", async (ctx) => {
   await ctx.scenes.enter("shogird");
 });
 
-
-bot.on("callback_query:data",async(ctx)=>{
-  ctx.reply("data")
-})
