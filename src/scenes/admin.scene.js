ec3e4aabe340414a6a58bf505e5559e81f5ea032
Abdurakhim254 <toxirmalk@gmail.com>
2025-04-23 18:06:34 +0500

    Just commit

--- src/scenes/admin.scene.js
+++ src/scenes/admin.scene.js
@@ -67,6 +67,7 @@ Adminscene.wait("button-actions").on("callback_query:data",async(ctx)=>{
         await ctx.reply(AdminSceneMessages.back,{
             reply_markup:backKeyboard
         })
+        ctx.scene.resume()
     })
     
 Adminscene.wait("back").on("callback_query:data",async(ctx)=>{
