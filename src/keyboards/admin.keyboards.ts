9dc997f00cc116c8c580511abf576a926dfa62f1
Abdurakhim254 <toxirmalk@gmail.com>
2025-04-27 22:36:01 +0500

    bot is ready

--- src/keyboards/admin.keyboards.ts
+++ src/keyboards/admin.keyboards.ts
@@ -8,6 +8,6 @@ export const Adminkeyboard = new InlineKeyboard().text(
 
 
 
-export const giveAddKeyboard = (id:number)=>{
-    return new InlineKeyboard().text("Tasdiqlash ✅",`tasdiq_${id}`).text("Bekor qilish ❌",`bekor_${id}`).row().text("Asosiy Menyuga qaytish ❌","back");
+export const giveAddKeyboard = (id:number,deleteid:number|undefined)=>{
+    return new InlineKeyboard().text("Tasdiqlash ✅",`tasdiq_${id}_${deleteid}`).text("Bekor qilish ❌",`bekor_${id}_${deleteid}`).row().text("Asosiy Menyuga qaytish ❌","back");
 };
--- src/scenes/admin.scene.ts
+++ src/scenes/admin.scene.ts
@@ -3,7 +3,7 @@ import { BotContext, NeededCount } from "../utils";
 import { APPLICATION } from "../config";
 import { AdminSceneMessages, SomeNeccessaryMessages } from "../messages";
 import { Adminkeyboard, giveAddKeyboard } from "../keyboards";
-import { FormatService, PostService } from "../helpers";
+import { FormatService, PostService, messageDeleter } from "../helpers";
 
 
 export const Adminscene = new Scene<BotContext>("admin");
@@ -14,6 +14,8 @@ const formatservice=new FormatService()
 Adminscene.step(async(ctx)=>{
 
     const id = (ctx as any).from.id;
+
+    (ctx as any).session.chatId = ctx.chat?.id;
 
     if(id!=APPLICATION.admin_id){
         await ctx.reply(AdminSceneMessages.forbidden)
@@ -41,7 +43,7 @@ Adminscene.wait("start").on("callback_query:data", async (ctx) => {
       for (const post of posts) {
         const format = await formatservice.createTemplate(post);
         await ctx.api.sendMessage(APPLICATION.admin_id, format, {
-          reply_markup: giveAddKeyboard(post.id),
+          reply_markup: giveAddKeyboard(post.id,ctx.msg?.message_id),
         });
       }
   
@@ -58,14 +60,17 @@ Adminscene.wait("start").on("callback_query:data", async (ctx) => {
 Adminscene.wait("give-add-ingore-add").on("callback_query:data",async(ctx)=>{
     const query = ctx.callbackQuery.data.toLowerCase().trim();
     const id=query.split("_")[1]
+    const deleteid=query.split("_")[2]
 
     if(query.startsWith(SomeNeccessaryMessages.accept)){
         await postservice.updatePost(+id)
+        await ctx.api.deleteMessage((ctx as any).session.chatId ,+deleteid)
         const post=await postservice.getPosts(NeededCount.ONE,+id)
         const format=await formatservice.createTemplate(post)
         await ctx.api.sendMessage(id,SomeNeccessaryMessages.good)
         await ctx.api.sendMessage(APPLICATION.channel,format)
     }else if(query.startsWith(SomeNeccessaryMessages.reject)){
+      await ctx.api.deleteMessage((ctx as any).session.chatId ,+deleteid)
         await postservice.deletePost(+id)
         await ctx.api.sendMessage(id,SomeNeccessaryMessages.bad)
     }else if(query==SomeNeccessaryMessages.back){
