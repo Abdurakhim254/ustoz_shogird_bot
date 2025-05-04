bae07b26d7ce341f0097abb5f7a9250ac27e6247
Abdurakhim254 <toxirmalk@gmail.com>
2025-05-04 23:01:02 +0500

    commit

--- src/scenes/ish.scene.ts
+++ src/scenes/ish.scene.ts
@@ -129,6 +129,8 @@ Ishscene.wait("last-middleware").on("message:text", async (ctx) => {
       }
     } catch (error) {
       const err = error as any;
+      console.log(err.message);
+      
       if (err.error_code === 403) {
         await ctx.reply(ErrorMessages.error);
       } else {
