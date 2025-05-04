c009e3afd387d9d57d8aced935a99f92589fbd9d
Abdurakhim254 <toxirmalk@gmail.com>
2025-05-04 17:29:10 +0500

    mini bug is fixed

--- src/scenes/admin.scene.ts
+++ src/scenes/admin.scene.ts
@@ -1,66 +1,66 @@
-import {Scene} from "grammy-scenes"
-import { BotContext, NeededCount } from "../utils";
-import { APPLICATION } from "../config";
-import { AdminSceneMessages, ButtonMessages, SomeNeccessaryMessages } from "../messages";
-import { Adminkeyboard, JobTypeKeyboards, giveAddKeyboard } from "../keyboards";
-import { FormatService, PostService, messageDeleter } from "../helpers";
+// import {Scene} from "grammy-scenes"
+// import { BotContext, NeededCount } from "../utils";
+// import { APPLICATION } from "../config";
+// import { AdminSceneMessages, ButtonMessages, SomeNeccessaryMessages } from "../messages";
+// import { Adminkeyboard, JobTypeKeyboards, giveAddKeyboard } from "../keyboards";
+// import { FormatService, PostService, messageDeleter } from "../helpers";
 
 
-export const Adminscene = new Scene<BotContext>("admin");
+// export const Adminscene = new Scene<BotContext>("admin");
 
-const postservice=new PostService()
-const formatservice=new FormatService()
+// const postservice=new PostService()
+// const formatservice=new FormatService()
 
-Adminscene.step(async(ctx)=>{
+// Adminscene.step(async(ctx)=>{
 
-    const id = (ctx as any).from.id;
-
-
-    if(id!=APPLICATION.admin_id){
-        await ctx.reply(AdminSceneMessages.forbidden)
-        return ctx.scene.exit()
-    }
-
-    await ctx.reply(AdminSceneMessages.Panel,{
-        reply_markup:Adminkeyboard
-    })
-    ctx.scene.resume()
-})
+//     const id = (ctx as any).from.id;
 
 
-Adminscene.wait("start").on("callback_query:data", async (ctx) => {
-    const query = ctx.callbackQuery.data.toLowerCase().trim();
+//     if(id!=APPLICATION.admin_id){
+//         await ctx.reply(AdminSceneMessages.forbidden)
+//         return ctx.scene.exit()
+//     }
 
-    if (query === SomeNeccessaryMessages.notify) {
-        await ctx.reply(AdminSceneMessages.addtypes,{
-          reply_markup:JobTypeKeyboards
-        })
-      }
+//     await ctx.reply(AdminSceneMessages.Panel,{
+//         reply_markup:Adminkeyboard
+//     })
+//     ctx.scene.resume()
+// })
+
+
+// Adminscene.wait("start").on("callback_query:data", async (ctx) => {
+//     const query = ctx.callbackQuery.data.toLowerCase().trim();
+
+//     if (query === SomeNeccessaryMessages.notify) {
+//         await ctx.reply(AdminSceneMessages.addtypes,{
+//           reply_markup:JobTypeKeyboards
+//         })
+//       }
       
-      ctx.scene.resume();
-  });
+//       ctx.scene.resume();
+//   });
   
 
 
-Adminscene.wait("give-add-by-types").on("callback_query:data",async(ctx)=>{
-    const query=ctx.callbackQuery.data.toLowerCase().trim()
-    if(query==ButtonMessages.ish){
+// Adminscene.wait("give-add-by-types").on("callback_query:data",async(ctx)=>{
+//     const query=ctx.callbackQuery.data.toLowerCase().trim()
+//     if(query==ButtonMessages.ish){
 
-    }else if(query==ButtonMessages.hodim){
+//     }else if(query==ButtonMessages.hodim){
 
-    }else if(query==ButtonMessages.ustoz){
+//     }else if(query==ButtonMessages.ustoz){
 
-    }else if(query==ButtonMessages.sherik){
+//     }else if(query==ButtonMessages.sherik){
 
-    }
+//     }
 
-    ctx.scene.resume()
-})
+//     ctx.scene.resume()
+// })
 
 
-Adminscene.wait("last-middleware").on("callback_query:data",async(ctx)=>{
+// Adminscene.wait("last-middleware").on("callback_query:data",async(ctx)=>{
   
-})
+// })
 
 
 
--- src/scenes/index.ts
+++ src/scenes/index.ts
@@ -1,6 +1,6 @@
 import { ScenesComposer } from 'grammy-scenes'
 import { HodimScene } from './hodim.scene'
-import { Adminscene } from './admin.scene'
+// import { Adminscene } from './admin.scene'
 import { Ishscene } from './ish.scene'
 import { Ustozscene } from './ustoz.scene'
 import { Sherikscene } from './sherik.scene'
@@ -10,7 +10,7 @@ import { BotContext } from '../utils'
 
 export const scenes=new ScenesComposer<BotContext>(
     HodimScene,
-    Adminscene,
+    // Adminscene,
     Ishscene,
     Ustozscene,
     Sherikscene
