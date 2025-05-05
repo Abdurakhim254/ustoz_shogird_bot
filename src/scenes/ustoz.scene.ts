75f9d8c3e4668f2981eac417db72ea52c995fcf9
Abdurakhim254 <toxirmalk@gmail.com>
2025-05-05 10:46:31 +0500

    check

--- src/scenes/ustoz.scene.ts
+++ src/scenes/ustoz.scene.ts
@@ -134,6 +134,8 @@ Ustozscene.wait("last-middleware").on("message:text", async (ctx) => {
       }
     } catch (error) {
       const err = error as any; 
+      console.log(err.message);
+      
       if (err.error_code === 403) {
         await ctx.reply(ErrorMessages.error);
       } else {
